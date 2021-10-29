73f9f54ca3e0852d2a6f37353118587e3be0e83b
niedzwiadek7 <damiankliber@gmail.com>
2021-10-30 00:31:28 +0200

    Create menu to user profile

---
+++ components/Profile/Recipes/Menu/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <form class="wrapper-form">
+    <div
+      v-for="(option, n) in possibleOptions"
+      :key="option"
+      class="wrapper-option"
+      :class="ischecked(option) && 'checked-option'"
+    >
+      <input
+        :id="option"
+        type="radio"
+        name="option"
+        class="input"
+        :checked="n === 0"
+        @change="$emit('input', option)"
+      >
+      <label
+        :for="option"
+        class="label"
+      >
+        {{ option }}
+      </label>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    possibleOptions: {
+      type: Array as PropType<Array<String>>,
+      required: true
+    }
+  },
+  methods: {
+    ischecked (value: String): boolean {
+      return value === this.value
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './assets/scss/predefine/position';
+@use './assets/scss/predefine/flex';
+@use './assets/scss/var';
+
+.wrapper-form {
+  display: flex;
+  flex-wrap: nowrap;
+  .wrapper-option {
+    position: relative;
+    width: 100%;
+    padding: 1.4em 0;
+    border-bottom: 1px solid var.$light-text-14;
+    .input {
+      width: 0;
+      height: 0;
+      visibility: hidden;
+    }
+    .label {
+      @include position.full-fill;
+      @include flex.center-center;
+      font-size: 1.3em;
+      font-weight: 600;
+      color: var.$light-text-7;
+    }
+    &::after {
+      content: '';
+      @include position.bottom-left(-1px, 0);
+      width: 100%;
+      border-bottom: 3px solid var.$main;
+      transform: scale(0);
+      transition: .2s ease-in-out;
+      will-change: transition;
+    }
+    &.checked-option {
+      .label {
+        color: var.$light-text-3;
+      }
+      &::after {
+        transform: scale(1);
+      }
+    }
+  }
+}
+</style>
