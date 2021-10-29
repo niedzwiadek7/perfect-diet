b12cb0bbbb37b7911093d698853b81d79c406984
niedzwiadek7 <damiankliber@gmail.com>
2021-10-29 12:52:49 +0200

    Create Menu for logged users
    New definitions for flex and position predefine
    Create list predefine stylesheet

---
+++ components/Layout/UserMenu/Element.vue
@@ -0,0 +1,56 @@
+<template>
+  <NuxtLink class="link" :to="link">
+    <slot name="icon" />
+    <div class="title">
+      {{ value }}
+    </div>
+  </NuxtLink>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    link: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './assets/scss/predefine/flex';
+@use './assets/scss/predefine/link';
+@use './assets/scss/var';
+
+.link {
+  @include flex.center-center(column);
+  @include link.reset(var.$light-text-7);
+  font-size: 1.4em;
+  .title {
+    display: none;
+    padding-top: .5em;
+  }
+  .icon {
+
+  }
+  &.nuxt-link-active {
+    color: var.$main;
+  }
+}
+
+@media (min-width: 576px) {
+  .link {
+    font-size: 1.1em;
+    .title {
+      display: block;
+    }
+  }
+}
+</style>
---
+++ components/Layout/UserMenu/Menu.vue
@@ -0,0 +1,123 @@
+<template>
+  <nav class="wrapper-menu">
+    <ul class="list-menu">
+      <li class="element-wrapper">
+        <LayoutUserMenuElement
+          link="/"
+          value="Profil"
+          class="wrapper"
+        >
+          <font-awesome-icon
+            slot="icon"
+            class="icon"
+            :icon="['fas', 'user']"
+          />
+        </LayoutUserMenuElement>
+      </li>
+
+      <li class="element-wrapper">
+        <LayoutUserMenuElement
+          link="/recipe/new"
+          value="Dodaj nowy"
+          class="wrapper"
+        >
+          <font-awesome-icon
+            slot="icon"
+            class="icon"
+            :icon="['fas', 'pencil-alt']"
+          />
+        </LayoutUserMenuElement>
+      </li>
+
+      <li class="element-wrapper">
+        <LayoutUserMenuElement
+          link="/menu"
+          value="Twoje menu"
+          class="wrapper"
+        >
+          <font-awesome-icon
+            slot="icon"
+            class="icon"
+            :icon="['fas', 'hamburger']"
+          />
+        </LayoutUserMenuElement>
+      </li>
+
+      <li class="element-wrapper">
+        <LayoutUserMenuElement
+          link="/notifications"
+          value="Powiadomienia"
+          class="wrapper"
+        >
+          <font-awesome-icon
+            slot="icon"
+            class="icon"
+            :icon="['fas', 'bell']"
+          />
+        </LayoutUserMenuElement>
+      </li>
+
+      <li class="element-wrapper">
+        <LayoutUserMenuElement
+          link="/shopping-list"
+          value="Lista zakupów"
+          class="wrapper"
+        >
+          <font-awesome-icon
+            slot="icon"
+            class="icon"
+            :icon="['fas', 'shopping-cart']"
+          />
+        </LayoutUserMenuElement>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      list: [
+        'Profil',
+        'Dodaj nowy',
+        'Menu',
+        'Powiadomienia',
+        'Zakupy'
+      ] as Array<string>
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './assets/scss/predefine/position';
+@use './assets/scss/predefine/flex';
+@use './assets/scss/predefine/zindex';
+@use './assets/scss/predefine/list';
+@use './assets/scss/var';
+
+.wrapper-menu {
+  @include position.fixed-bottom-left(0, 0);
+  background-color: var.$light-text-16;
+  width: 100%;
+  padding: {
+    bottom: 1em;
+    top: 1em;
+  }
+  z-index: zindex.$z-index-10;
+  .list-menu {
+    @include flex.around-center;
+    @include list.reset;
+    .element-wrapper {
+      .wrapper {
+        .icon {
+          font-size: 1.5em;
+        }
+      }
+    }
+  }
+}
+</style>
