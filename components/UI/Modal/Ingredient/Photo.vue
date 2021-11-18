8397f708a8e2643ba51f405cf281c769f233e13a
niedzwiadek7 <damiankliber@gmail.com>
2021-11-18 10:39:20 +0100

    Create photo element for Ingredient Modal

---
+++ components/UI/Modal/Ingredient/Photo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper-img">
+    <div class="container-img">
+      <img
+        :src="photo || require('~/assets/images/NoPhoto/Ingredient.jpg')"
+        alt=""
+        class="image"
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  props: {
+    photo: {
+      type: undefined as unknown as PropType<(File | undefined)>, /* FIXME make concrete type prop value */
+      required: false,
+      default () {
+        return undefined
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './assets/scss/predefine/flex';
+
+.wrapper-img {
+  @include flex.center-center;
+  margin: {
+    left: auto;
+    right: auto;
+  }
+  width: 9em;
+  height: 9em;
+  background: linear-gradient(0deg, rgba(169,169,169,1) 0%, rgba(254,254,254,1) 63%);
+  border-radius: 50%;
+  .container-img {
+    @include flex.center-center;
+    width: 8em;
+    height: 8em;
+    border-radius: 50%;
+    .image {
+      width: 8em;
+      height: 8em;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
