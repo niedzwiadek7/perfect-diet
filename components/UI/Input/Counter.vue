b9100b2f5bfb7655186784437a0dee5a5f068e43
niedzwiadek7 <damiankliber@gmail.com>
2021-11-18 09:34:11 +0100

    Create Input Counter Element

---
+++ components/UI/Input/Counter.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper-Counter">
+    <div class="wrapper-meter">
+      <div
+        class="minus-symbol"
+        @click="$emit('input', value - 1)"
+      >
+        -
+      </div>
+      <input
+        :value="value"
+        type="number"
+        class="input-count"
+        readonly
+      >
+      <div
+        class="plus-symbol"
+        @click="$emit('input', value + 1)"
+      >
+        +
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  watch: {
+    value: {
+      handler (newValue: number) {
+        if (newValue < 0) {
+          this.$emit('input', 0)
+        }
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use './assets/scss/predefine/flex';
+@use './assets/scss/var';
+
+.wrapper-meter {
+  @include flex.center-center;
+  gap: 1.5em;
+  .minus-symbol {
+    font-size: 1.5em;
+    font-weight: 700;
+  }
+  .input-count {
+    width: 100%;
+    background: transparent;
+    text-align: center;
+    border: none;
+    border-bottom: 2px solid var.$main;
+    outline: none;
+  }
+  .plus-symbol {
+    font-size: 1.5em;
+    font-weight: 700;
+  }
+}
+</style>
