6e4f07f97dabf571ced5e5e667ec4b6f37320482
niedzwiadek7 <damiankliber@gmail.com>
2022-02-02 01:55:38 +0100

    Create main layout for notification

---
+++ components/Notifications/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper-index">
+    <NotificationsSection
+      v-for="section in notifications"
+      :key="section.title"
+      :section="section"
+      class="notification"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+
+export default Vue.extend({
+  computed: {
+    ...mapGetters({
+      notifications: 'notification/getList'
+    })
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.wrapper-index {
+  margin-top: 1.5em;
+  .notification {
+    margin: {
+      left: auto;
+      right: auto;
+    }
+    width: 90%;
+  }
+}
+</style>
