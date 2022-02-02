0c4c22a89d9fdcb6ed09b3ca9e388a597f56b552
niedzwiadek7 <damiankliber@gmail.com>
2022-02-02 01:59:21 +0100

    Create full layout for notifications

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -1,17 +1,60 @@
 <template>
-    <div class="wrapper-notifications">
-
+  <client-only>
+    <div
+      v-if="$fetchState.pending"
+      class="full-fill"
+    >
+      <UILoading
+        class="loading"
+      />
     </div>
+
+    <div
+      v-else-if="$fetchState.error"
+    >
+      <UIErrorsNotFound
+        v-if="$fetchState.error.response.status === 404"
+        class="not-found"
+      />
+      <UIErrorsServerError
+        v-else
+        class="server-error"
+      />
+    </div>
+
+    <Notifications
+      v-else
+    />
+  </client-only>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-
+  layout: 'UserMenu',
+  async fetch (): Promise<void> {
+    const { store, app } = this.$nuxt.context
+    await store.dispatch('notification/getNotifications', {
+      app
+    })
+  }
 })
 </script>
 
 <style lang="scss" scoped>
+@use './assets/scss/predefine/flex';
+@use './assets/scss/predefine/position';
+@use './assets/scss/var';
 
+.full-fill {
+  @include position.full-fill;
+  @include flex.center-center;
+  width: 100vw;
+  height: 100vh;
+  .loading {
+    --color: #1FCC79; // FIXME should be SCSS property var.$main
+    --size: 8px;
+  }
+}
 </style>
